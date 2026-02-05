4258e56496c7795fa2bf576f4b2fe87726a4720a
Hummingliberty616 <wlin2028@kehillahstudent.org>
2026-02-04 20:06:56 -0800

    -

--- main.py
+++ main.py
@@ -7,15 +7,25 @@
 #
 #  Initialize your ciphertext an empty string
 
+def CaesarCipher (plaintext):
+    plaintext = plaintext
+    ciphertext = ""
+    for character in plaintext:
+        number = ord(character)
+        encrypted_character = chr(number+6)
+        ciphertext += encrypted_character
+    print(ciphertext)
+
 plaintext = "This is a secret message."
 ciphertext = ""
 for character in plaintext:
     # do something to the character to encrypt it
     # YOUR CODE HERE
     number = ord(character)
-    ascii_letter = chr(number+6)
-    encrypted_character = ascii_letter # CHANGE THIS!
+    encrypted_character = chr(number+6)# CHANGE THIS!
     ciphertext += encrypted_character
 
 print(ciphertext)
+
+CaesarCipher("This is a secret message.")
 
